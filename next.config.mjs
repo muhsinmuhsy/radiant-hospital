9ce1677f9ff1aa74405427bb273c5ea764749994
muhsinmuhsy <muhsinkoodali856@gmail.com>
2025-01-22 00:47:00 +0530

    kk]

--- next.config.mjs
+++ next.config.mjs
@@ -1,34 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-      remotePatterns: [
-        {
-          protocol: '*',
-          hostname: '**',
-          port: '',
-          pathname: '/**',
-        },
-        // {
-        //   protocol: 'http',
-        //   hostname: '127.0.0.1',
-        //   port: '',
-        //   pathname: '/**',
-        // },
-        // {
-        //   protocol: 'http',
-        //   hostname: 'localhost',
-        //   port: '',
-        //   pathname: '/**',
-        // },
-        // {
-        //   protocol: 'https',
-        //   hostname: 'muhsiiyy.pythonanywhere.com',
-        //   port: '',
-        //   pathname: '/**',
-        // },
-      ],
-    },
-  };
-  
-  export default nextConfig;
-  
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '*',
+        pathname: '**',
+      },
+      {
+        protocol: 'http',
+        hostname: '*',
+        pathname: '**',
+      },
+    ],
+  },
+}
+
+export default nextConfig
