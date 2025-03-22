2d54290083c47b384586c4430da99ffe49cdd35b
faslimpachu <faslimpachu@gmail.com>
2025-03-23 03:14:02 +0530

    updated sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,15 +1,18 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-    siteUrl: 'https://www.radiantenthospital.com', // Change this to your website URL
-    generateRobotsTxt: true, // Generates a robots.txt file
-    sitemapSize: 5000, // Default is fine
-    exclude: ['/admin', '/dashboard'], // Exclude private pages
+    siteUrl: "https://www.radiantenthospital.com", // Your domain
+    generateRobotsTxt: true, // Generate robots.txt
+    sitemapSize: 5000,
+    exclude: ["/404", "/admin"], // Exclude unnecessary pages
     robotsTxtOptions: {
       policies: [
         {
-          userAgent: '*',
-          allow: '/',
+          userAgent: "*",
+          allow: "/",
         },
+      ],
+      additionalSitemaps: [
+        "https://www.radiantenthospital.com/sitemap-0.xml", // Ensure this exists
       ],
     },
   };
